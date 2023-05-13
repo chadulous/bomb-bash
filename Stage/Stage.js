170ce9fd397de7f1faf319b7a76929fe4d075cfa
Restyled.io <commits@restyled.io>
2023-05-13 15:58:28 +0000

    Restyled by clang-format

--- Stage/Stage.js
+++ Stage/Stage.js
@@ -1,12 +1,12 @@
 /* eslint-disable require-yield, eqeqeq */
 
 import {
+  Color,
+  Costume,
+  Sound,
   Stage as StageBase,
   Trigger,
   Watcher,
-  Costume,
-  Color,
-  Sound,
 } from "https://unpkg.com/leopard@^1/dist/index.esm.js";
 
 export default class Stage extends StageBase {
@@ -16,16 +16,16 @@ export default class Stage extends StageBase {
     this.vars.proportions = prop;
     this.costumes = [
       new Costume("backdrop1", "./Stage/costumes/backdrop1.svg", {
-        x: 240,
-        y: 180,
+        x : 240,
+        y : 180,
       }),
     ];
 
-    this.sounds = [new Sound("pop", "./Stage/sounds/pop.wav")];
+    this.sounds = [ new Sound("pop", "./Stage/sounds/pop.wav") ];
 
-    this.triggers = [new Trigger(Trigger.GREEN_FLAG, this.loop)];
+    this.triggers = [ new Trigger(Trigger.GREEN_FLAG, this.loop) ];
   }
-  *loop() {
+  * loop() {
     while (true) {
       console.log(this.mouse.x, this.mouse.y);
       yield;
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import { Project } from "https://unpkg.com/leopard@^1/dist/index.esm.js";
+import {Project} from "https://unpkg.com/leopard@^1/dist/index.esm.js";
 
-import Stage from "./Stage/Stage.js";
-import Stone from "./Stone/Stone.js";
-import Sand from "./Sand/Sand.js";
 import Bomb from "./Bomb/Bomb.js";
 import ExplosionParticle from "./ExplosionParticle/ExplosionParticle.js";
+import Sand from "./Sand/Sand.js";
+import Stage from "./Stage/Stage.js";
+import Stone from "./Stone/Stone.js";
 
 const gameSize = 1;
 const base_height = 216;
@@ -13,51 +13,49 @@ const height = gameSize * base_height;
 const width = gameSize * base_width;
 const blockSize = 18 * gameSize;
 
-const stage = new Stage(
-  { height, width, costumeNumber: 1 },
-  { width, height, gameSize, blockSize }
-);
+const stage = new Stage({height, width, costumeNumber : 1},
+                        {width, height, gameSize, blockSize});
 noise.seed(Math.round(Math.random()) * 65536);
 const sprites = {
-  Stone: new Stone({
-    x: -231,
-    y: 171,
-    direction: 90,
-    costumeNumber: 1,
-    size: 100 * gameSize,
-    visible: true,
-    layerOrder: 4,
+  Stone : new Stone({
+    x : -231,
+    y : 171,
+    direction : 90,
+    costumeNumber : 1,
+    size : 100 * gameSize,
+    visible : true,
+    layerOrder : 4,
   }),
-  Sand: new Sand({
-    x: -231,
-    y: 171,
-    direction: 90,
-    costumeNumber: 1,
-    size: 100 * gameSize,
-    visible: true,
-    layerOrder: 3,
+  Sand : new Sand({
+    x : -231,
+    y : 171,
+    direction : 90,
+    costumeNumber : 1,
+    size : 100 * gameSize,
+    visible : true,
+    layerOrder : 3,
   }),
-  Bomb: new Bomb({
-    x: -42,
-    y: -81,
-    direction: 90,
-    costumeNumber: 1,
-    size: 100 * gameSize,
-    visible: true,
-    layerOrder: 2,
+  Bomb : new Bomb({
+    x : -42,
+    y : -81,
+    direction : 90,
+    costumeNumber : 1,
+    size : 100 * gameSize,
+    visible : true,
+    layerOrder : 2,
   }),
-  ExplosionParticle: new ExplosionParticle({
-    x: -60,
-    y: -36,
-    direction: 90,
-    costumeNumber: 1,
-    size: 100 * gameSize,
-    visible: true,
-    layerOrder: 1,
+  ExplosionParticle : new ExplosionParticle({
+    x : -60,
+    y : -36,
+    direction : 90,
+    costumeNumber : 1,
+    size : 100 * gameSize,
+    visible : true,
+    layerOrder : 1,
   }),
 };
 
 const project = new Project(stage, sprites, {
-  frameRate: 30, // Set to 60 to make your project run faster
+  frameRate : 30, // Set to 60 to make your project run faster
 });
 export default project;
